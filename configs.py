65c5fc7fb3829592e997be1f99d924ab7a7dfaf1
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-13 16:28:44 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,9 @@
+from os import path, getenv
+
+class Config:
+    API_ID = int(getenv("API_ID", "0112234"))
+    API_HASH = getenv("API_HASH", "abcdefg")
+    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    MONGO_URI = getenv("MONGO_URI", "")
+
+cfg = Config()
