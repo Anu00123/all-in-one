6a0d1aeb1101c86bc1689f29da7b09e4e36dfb0b
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-14 19:51:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors.exceptions.flood_420 import FloodWait
 from configs import cfg
 from database import add_user, add_group, all_users, all_groups, users, remove_user
 
