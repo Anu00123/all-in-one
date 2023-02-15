c7aade66d46d3f9a9ec024238a9bdae54cd9456f
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-15 18:31:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,6 +4,6 @@ class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://a:a@cluster0.x6vmjau.mongodb.net/?retryWrites=true&w=majority")
 
 cfg = Config()
