25c635bb11bfdab1d8668337422935fc0bfeb8d8
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-15 19:43:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from configs import cfg
 from database import add_user, add_group, all_users, all_groups, users, remove_user
 from pyrogram.errors import UserNotParticipant
 import random, asyncio
+import time
 
 
 API_ID = "13197673"
@@ -132,6 +133,18 @@ async def help_cmd(client, message):
         reply_markup=InlineKeyboardMarkup(START_BUTTONS)
     )
 
+
+
+
+#-------ping------
+
+@app.on_message(filters.command("ping"))
+async def ping_cmd(client, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
 
 
