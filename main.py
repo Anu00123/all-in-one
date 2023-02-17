d7d3e9615189d79a966e968852d24b23756fa033
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-17 18:24:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from database import add_user, add_group, all_users, all_groups, users, remove_u
 from pyrogram.errors import UserNotParticipant
 import random, asyncio
 import time
+from helper_func import subscribed, encode, decode, get_messages
 
 
 API_ID = "13197673"
