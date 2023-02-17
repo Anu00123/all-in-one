e61315f30bcafbccaac2940ce2a0f4e1e612ce14
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-17 14:25:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,5 +6,6 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://a:a@cluster0.x6vmjau.mongodb.net/?retryWrites=true&w=majority")
     FORCE_SUB_CHANNEL = getenv("FORCE_SUB_CHANNEL", "-1001824579385")
+    ADMINS = getenv("ADMINS", "5686364473")
 
 cfg = Config()
