c9db9efe5fc30e5358009c8cb69d2a4155480541
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-18 08:57:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters, 
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ChatJoinRequest, Message
 from pyrogram.errors.exceptions.flood_420 import FloodWait
 from configs import cfg
