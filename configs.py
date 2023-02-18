ca80e13e687105ba7179601af9dcb33fab6ddac4
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-18 10:17:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,6 @@ class Config:
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://a:a@cluster0.x6vmjau.mongodb.net/?retryWrites=true&w=majority")
     FORCE_SUB_CHANNEL = (getenv("FORCE_SUB_CHANNEL", "-1001824579385"))
     ADMINS = getenv("ADMINS", "5686364473")
-    custom_caption = getenu("", "")
+    custom_caption = getenu("custom_caption", "{file_name}")
 
 cfg = Config()
