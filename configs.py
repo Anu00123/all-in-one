df634386015bbe413e6d29513f0c1c04cfeb90ac
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-19 10:11:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "abcdefg")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://a:a@cluster0.x6vmjau.mongodb.net/?retryWrites=true&w=majority")
-    FORCE_SUB_CHANNEL = (getenv("FORCE_SUB_CHANNEL", "-1001824579385"))
+    CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001824579385"))
     ADMINS = getenv("ADMINS", "5686364473")
     custom_caption = getenu("custom_caption", "{file_name}")
 
