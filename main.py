8dfe513021b3cf1842d0a8783847a8cf43485fb6
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-19 10:08:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -151,42 +151,9 @@ async def ping_cmd(client, message):
 
 
 
-#------autocaption-------
+#------filesharing bot-------
 
 
-@app.on_message(pyrogram.filters.channel)
-def edit_caption(bot, update: pyrogram.types.Message):
-  if os.environ.get("custom_caption")
-      motech, _ = get_file_details(update)
-      try:
-          try: update.edit(custom_caption.format(file_name=motech.file_name))
-          except pyrogram.errors.FloodWait as FloodWait:
-              asyncio.sleep(FloodWait.value)
-              update.edit(custom_caption.format(file_name=motech.file_name, mote))
-      except pyrogram.errors.MessageNotModified: pass 
-  else:
-      return
-    
-def get_file_details(update: pyrogram.types.Message):
-  if update.media:
-    for message_type in (
-        "photo",
-        "animation",
-        "audio",
-        "document",
-        "video",
-        "video_note",
-        "voice",
-        # "contact",
-        # "dice",
-        # "poll",
-        # "location",
-        # "venue",
-        "sticker"
-    ):
-        obj = getattr(update, message_type)
-        if obj:
-            return obj, obj.file_id
 
 
 
