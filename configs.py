4ec851f5eab63c4c9f1128e2540eafcd7286ae17
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-19 22:22:30 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,9 +5,6 @@ class Config:
     API_HASH = getenv("API_HASH", "abcdefg")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://a:a@cluster0.x6vmjau.mongodb.net/?retryWrites=true&w=majority")
-    CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001824579385"))
-    ADMINS = getenv("ADMINS", "5686364473")
-   
-
+    
 
 cfg = Config()
