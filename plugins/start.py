c65803e900fe9efb59254c51930b9cf59dc5504b
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-19 10:40:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -112,4 +112,17 @@ async def approve(client, message: ChatJoinRequest):
 
 
 
+#-------ping------
+
+@app.on_message(filters.command("ping"))
+async def ping_cmd(client, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
+
+
+
+
 
