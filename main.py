e0183cc3c628884adc0b78f10e65680f6cbb20e6
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-19 11:13:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ChatJoinRequest, Message
 from pyrogram.errors.exceptions.flood_420 import FloodWait
-from configs import cfg, ADMINS
+from configs import cfg
 from database import add_user, add_group, all_users, all_groups, users, remove_user
 from pyrogram.errors import UserNotParticipant
 import random, asyncio
 import time
-
+from configs import anuten
 
 
 API_ID = "13197673"
