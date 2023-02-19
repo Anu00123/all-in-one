3359b2e2016d26b3c80e77a6ea57b80a12753102
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2023-02-19 10:54:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = Client(
     )
 
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+@app.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
